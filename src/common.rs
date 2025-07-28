826fb462f1dd61d961571f747c567c8d10f21733
fymsc <dengjiejz@126.com>
2025-07-28 20:19:45 +0800

    修改替换API地址bwg

--- src/common.rs
+++ src/common.rs
@@ -1004,7 +1004,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://yc.niani.top:21114".to_owned()
+    "http://bwgyc.itsk.top:21114".to_owned()
 }
 
 #[inline]
