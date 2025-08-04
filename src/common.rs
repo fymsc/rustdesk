6f244630d0136868b184ab0b16fe7b3f39325325
fymsc <dengjiejz@126.com>
2025-08-04 14:44:03 +0800

    Change bwg api

--- src/common.rs
+++ src/common.rs
@@ -1004,7 +1004,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://yc.niani.top:21114".to_owned()
+    "http://yc.itsk.top:31114".to_owned()
 }
 
 #[inline]
